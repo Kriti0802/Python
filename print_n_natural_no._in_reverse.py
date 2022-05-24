454dc365ed7eda7446dca097526c86b00519ccba
Kriti Singh <90671322+Kriti0802@users.noreply.github.com>
2022-05-24 11:33:48 +0530

    Add files via upload

---
+++ print_n_natural_no._in_reverse.py
@@ -0,0 +1,12 @@
+i=int(input("Enter the no.= "))
+
+while(i>=1):
+    print(i)
+    i=i-1
+    
+    
+#print from n to i
+n=int(input("Enter the no upto you want to print= "))
+while(n>=1):
+    print(n)
+    n=n-1
