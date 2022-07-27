4b47d225e3fdbda056b7e7a302efd2dd988bdc8c
Kriti Singh <90671322+Kriti0802@users.noreply.github.com>
2022-07-27 22:12:12 +0530

    Add files via upload

---
+++ add,sub,mul.py
@@ -0,0 +1,10 @@
+a=int(input("Enter 1st number= "))
+b=int(input("Enter 2nd number= "))
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("add= ",add)
+print("sub= ",sub)
+print("mul= ",mul)
+print("div= ",div)
